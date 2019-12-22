4b145da645cf6e829b6d694162885d31b0648d27
Łukasz <ukasz@DESKTOP-R9QDOGC>
2019-12-22 21:09:00 +0100

    q

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.NetworkInformation;
+
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Ping ping = new Ping();
+            MessageBox.Show(test1.Text);
+            //PingReply.
+            PingReply reply = ping.Send(test1.Text, 1000);
+            MessageBox.Show(reply.Status.ToString());
+        }
+
+
+    }
+}
